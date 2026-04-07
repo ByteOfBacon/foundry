45c446940ec5af8bad8f8122e06722b39405d9f3
Kairi Hang <byteofbacon@protonmail.com>
2026-04-06 18:22:39 -0600

    i don't know why db is undefined.

--- scripts/src/utils/overpass.js
+++ scripts/src/utils/overpass.js
@@ -75,6 +75,9 @@ export async function runQueryOverture(query) {
     await db.run(`INSTALL httpfs;`)
     await db.run(`SET s3_region='us-west-2';`); //change for your region
     const data = await db.runAndReadAll(query); //should return geojson
+    if (!data) {
+      throw new Error('No data returned from Overture query');
+    }
     return data;
   } catch (err) {
     console.error('Error running Overture query:', query, err);
